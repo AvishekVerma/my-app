a5829ddf0e2e1697bcc40eb8f92cceacaae44b84
Avishek <avishekverma298@gmail.com>
2022-01-05 19:26:13 +0530

    Tic tac toe game added

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
-// import logo from './logo.svg';
-import profileImg from './img/avi-img.png';
+import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>My React App</h1>
-      <h2>Author : Avishek Kumar Verma</h2>
-      <img src={profileImg} alt="profile-image" />
-
-      {/* <header className="App-header">
+      <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -22,7 +17,7 @@ function App() {
         >
           Learn React
         </a>
-      </header> */}
+      </header>
     </div>
   );
 }
